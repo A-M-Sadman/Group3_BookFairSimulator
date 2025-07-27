2ca603c19d5453b6aec34d02720f185e11a5b105
A-M-Sadman <abdullahsadman2004@gmail.com>
2025-07-28 00:48:04 +0600

    Added Maintenance Officer Dashboard

--- src/main/java/com/groupthree/group3_bookfairsimulator/AbdullahMohammadSadman/StallOwnerDashboardController.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/AbdullahMohammadSadman/StallOwnerDashboardController.java
@@ -15,15 +15,27 @@ public class StallOwnerDashboardController
     }
 
     @javafx.fxml.FXML
-    public void customerQueries(ActionEvent actionEvent) {
+    public void customerQueries(ActionEvent actionEvent) throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("AbdullahMohammadSadman/CustomerQueries.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("FAQs");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void recordPurchase(ActionEvent actionEvent) {
+    public void recordPurchase(ActionEvent actionEvent) throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("AbdullahMohammadSadman/RecordPurchase.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Purchase Record");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void viewCatalog(ActionEvent actionEvent) {
+    public void viewCatalog(ActionEvent actionEvent) throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("AbdullahMohammadSadman/ViewCatalog.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Catalogue");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
@@ -34,19 +46,35 @@ public class StallOwnerDashboardController
     }
 
     @javafx.fxml.FXML
-    public void checkStock(ActionEvent actionEvent) {
+    public void checkStock(ActionEvent actionEvent) throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("AbdullahMohammadSadman/CheckStock.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Check Stock Quantity");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void applyDiscount(ActionEvent actionEvent) {
+    public void applyDiscount(ActionEvent actionEvent) throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("AbdullahMohammadSadman/ApplyDiscount.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Apply Discount");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void viewSales(ActionEvent actionEvent) {
+    public void viewSales(ActionEvent actionEvent) throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("AbdullahMohammadSadman/ViewSales.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("View Sales");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void updatePrice(ActionEvent actionEvent) {
+    public void updatePrice(ActionEvent actionEvent) throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("AbdullahMohammadSadman/UpdatePrice.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Update Price");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
