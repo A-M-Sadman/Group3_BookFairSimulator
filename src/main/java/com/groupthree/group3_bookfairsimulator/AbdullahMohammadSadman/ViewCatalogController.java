461736bdec3a2eaf37d0016c86d53f5bd54d3c26
A-M-Sadman <abdullahsadman2004@gmail.com>
2025-07-28 20:41:17 +0600

    added View Catalog fxml

--- src/main/java/com/groupthree/group3_bookfairsimulator/AbdullahMohammadSadman/ViewCatalogController.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/AbdullahMohammadSadman/ViewCatalogController.java
@@ -1,7 +1,52 @@
 package com.groupthree.group3_bookfairsimulator.AbdullahMohammadSadman;
 
+import com.groupthree.group3_bookfairsimulator.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+import static com.groupthree.group3_bookfairsimulator.AbdullahMohammadSadman.Book.bookList;
+
 public class ViewCatalogController
 {
     @javafx.fxml.FXML
+    private TableColumn<Book, String> titleCol;
+    @javafx.fxml.FXML
+    private TableColumn<Book, String> genreCol;
+    @javafx.fxml.FXML
+    private TableColumn<Book, String> authorCol;
+    @javafx.fxml.FXML
+    private TableColumn<Book, Integer> quantityCol;
+    @javafx.fxml.FXML
+    private TableView<Book> bookTableView;
+    @javafx.fxml.FXML
+    private TableColumn<Book, Double> priceCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        authorCol.setCellValueFactory(new PropertyValueFactory<>("author"));
+        priceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+        quantityCol.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        genreCol.setCellValueFactory(new PropertyValueFactory<>("genre"));
+
+        bookTableView.getItems().addAll(bookList);
+    }
+
+    @javafx.fxml.FXML
+    public void show(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void back(ActionEvent actionEvent) throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("AbdullahMohammadSadman/StallOwnerDashboard.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setScene(scene);
+    }
+}
