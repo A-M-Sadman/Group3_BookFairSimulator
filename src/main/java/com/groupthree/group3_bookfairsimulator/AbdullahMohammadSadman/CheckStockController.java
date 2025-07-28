6acacc2124c654511b4e1708694c9dfc3e1b8d3d
A-M-Sadman <abdullahsadman2004@gmail.com>
2025-07-28 20:29:19 +0600

    added Check Stock fxml

--- src/main/java/com/groupthree/group3_bookfairsimulator/AbdullahMohammadSadman/CheckStockController.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/AbdullahMohammadSadman/CheckStockController.java
@@ -1,7 +1,49 @@
 package com.groupthree.group3_bookfairsimulator.AbdullahMohammadSadman;
 
+import com.groupthree.group3_bookfairsimulator.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+import static com.groupthree.group3_bookfairsimulator.AbdullahMohammadSadman.Book.bookList;
+
 public class CheckStockController
 {
     @javafx.fxml.FXML
+    private TableColumn<Book, String> titleCol;
+    @javafx.fxml.FXML
+    private TableColumn<Book, String> authorCol;
+    @javafx.fxml.FXML
+    private Label showStockLabel;
+    @javafx.fxml.FXML
+    private TableView<Book> bookTableView;
+    @javafx.fxml.FXML
+    private TableColumn<Book, Double> priceCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        authorCol.setCellValueFactory(new PropertyValueFactory<>("author"));
+        priceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        bookTableView.getItems().addAll(bookList);
+    }
+
+    @javafx.fxml.FXML
+    public void back(ActionEvent actionEvent) throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("AbdullahMohammadSadman/StallOwnerDashboard.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setScene(scene);
+    }
+
+    @javafx.fxml.FXML
+    public void showStock(ActionEvent actionEvent) {
+    }
+}
