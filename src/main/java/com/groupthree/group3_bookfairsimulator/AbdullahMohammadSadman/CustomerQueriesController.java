32e04a59e794fc908c4dc1d564e0609aa973d767
A-M-Sadman <abdullahsadman2004@gmail.com>
2025-07-28 21:42:40 +0600

    Added Customer Query model class

--- src/main/java/com/groupthree/group3_bookfairsimulator/AbdullahMohammadSadman/CustomerQueriesController.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/AbdullahMohammadSadman/CustomerQueriesController.java
@@ -1,7 +1,51 @@
 package com.groupthree.group3_bookfairsimulator.AbdullahMohammadSadman;
 
+import com.groupthree.group3_bookfairsimulator.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+import static com.groupthree.group3_bookfairsimulator.AbdullahMohammadSadman.CustomerQuery.queryList;
+
 public class CustomerQueriesController
 {
     @javafx.fxml.FXML
+    private TableColumn<CustomerQuery, String> messageCol;
+    @javafx.fxml.FXML
+    private TableColumn<CustomerQuery, String> queryIdCol;
+    @javafx.fxml.FXML
+    private TableView<CustomerQuery> queryTableView;
+    @javafx.fxml.FXML
+    private Label replyLabel;
+    @javafx.fxml.FXML
+    private TableColumn<CustomerQuery, String> customerNameCol;
+    @javafx.fxml.FXML
+    private TextArea messageArea;
+    @javafx.fxml.FXML
+    private TextField replyInput;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+        queryIdCol.setCellValueFactory(new PropertyValueFactory<>("customerId"));
+        customerNameCol.setCellValueFactory(new PropertyValueFactory<>("customerName"));
+        messageCol.setCellValueFactory(new PropertyValueFactory<>("message"));
+
+        queryTableView.getItems().addAll(queryList);
+    }
+
+    @javafx.fxml.FXML
+    public void back(ActionEvent actionEvent) throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("AbdullahMohammadSadman/StallOwnerDashboard.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setScene(scene);
+    }
+
+    @javafx.fxml.FXML
+    public void reply(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/groupthree/group3_bookfairsimulator/AbdullahMohammadSadman/CustomerQuery.java
@@ -0,0 +1,50 @@
+package com.groupthree.group3_bookfairsimulator.AbdullahMohammadSadman;
+
+import java.util.ArrayList;
+
+public class CustomerQuery {
+    private String customerId;
+    private String customerName;
+    private String message;
+
+    protected static ArrayList<CustomerQuery> queryList = new ArrayList<>();
+
+    public CustomerQuery(String customerId, String customerName, String message) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.message = message;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerQuery{" +
+                "customerId='" + customerId + '\'' +
+                ", customerName='" + customerName + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
