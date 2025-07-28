f1123e6c9bc4754ceb542afa4c607d2798ad51b1
A-M-Sadman <abdullahsadman2004@gmail.com>
2025-07-28 20:06:45 +0600

    Updated update price controller

--- src/main/java/com/groupthree/group3_bookfairsimulator/AbdullahMohammadSadman/ViewSalesController.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/AbdullahMohammadSadman/ViewSalesController.java
@@ -1,7 +1,31 @@
 package com.groupthree.group3_bookfairsimulator.AbdullahMohammadSadman;
 
+import com.groupthree.group3_bookfairsimulator.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class ViewSalesController
 {
     @javafx.fxml.FXML
+    private Label messageLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void showTotalSales(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void back(ActionEvent actionEvent) throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("AbdullahMohammadSadman/StallOwnerDashboard.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setScene(scene);
+    }
+}
