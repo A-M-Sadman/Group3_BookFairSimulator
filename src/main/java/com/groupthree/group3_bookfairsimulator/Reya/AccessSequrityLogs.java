858f126b1f07854d3aabc0d8e7a93b01579d7572
MadMax-cpu <sajiborton6437@gamil.com>
2025-08-04 15:43:29 +0600

    add something

--- src/main/java/com/groupthree/group3_bookfairsimulator/Reya/AccessSequrityLogs.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/Reya/AccessSequrityLogs.java
@@ -1,7 +1,38 @@
 package com.groupthree.group3_bookfairsimulator.Reya;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 public class AccessSequrityLogs
 {
     @javafx.fxml.FXML
+    private DatePicker date;
+    @javafx.fxml.FXML
+    private TableColumn datecombo;
+    @javafx.fxml.FXML
+    private TableColumn locationCombo;
+    @javafx.fxml.FXML
+    private TextField logIdTextField;
+    @javafx.fxml.FXML
+    private TextField nameTextField;
+    @javafx.fxml.FXML
+    private TextField locationTextField;
+    @javafx.fxml.FXML
+    private TableColumn nameCombo;
+    @javafx.fxml.FXML
+    private TableColumn logIdCombo;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void addLogButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/groupthree/group3_bookfairsimulator/Reya/AssignDuty.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/Reya/AssignDuty.java
@@ -1,7 +1,41 @@
 package com.groupthree.group3_bookfairsimulator.Reya;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class AssignDuty
 {
     @javafx.fxml.FXML
+    private TextField idTextField;
+    @javafx.fxml.FXML
+    private TextField currentDutyTextField;
+    @javafx.fxml.FXML
+    private ComboBox dutyType;
+    @javafx.fxml.FXML
+    private TableColumn idCombo;
+    @javafx.fxml.FXML
+    private TableColumn dutyComboBox;
+    @javafx.fxml.FXML
+    private TableColumn locationCombox;
+    @javafx.fxml.FXML
+    private TextField nameTextField;
+    @javafx.fxml.FXML
+    private TableColumn currentDutyCombo;
+    @javafx.fxml.FXML
+    private TableView tableView;
+    @javafx.fxml.FXML
+    private TextField locationTextField;
+    @javafx.fxml.FXML
+    private TableColumn nameCombo;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void addDutyButton(ActionEvent actionEvent) {
+    }
+}
