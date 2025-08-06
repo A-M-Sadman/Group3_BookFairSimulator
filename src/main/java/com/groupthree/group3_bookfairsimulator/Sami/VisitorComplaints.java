f22a7f72defe4d2fea0dcfa65ab0f6bd54ba0714
tenacioussami <mdfarhanhossainsami@gmail.com>
2025-08-06 23:17:28 +0600

    update fxml 9

--- src/main/java/com/groupthree/group3_bookfairsimulator/Sami/VisitorComplaints.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/Sami/VisitorComplaints.java
@@ -1,7 +1,40 @@
 package com.groupthree.group3_bookfairsimulator.Sami;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class VisitorComplaints
 {
     @javafx.fxml.FXML
+    private TableColumn visitorNameCol;
+    @javafx.fxml.FXML
+    private TextField visitorIdTextField;
+    @javafx.fxml.FXML
+    private TableColumn visitorContactCol;
+    @javafx.fxml.FXML
+    private TextField visitorCntactTextField;
+    @javafx.fxml.FXML
+    private TableColumn complaintsCol;
+    @javafx.fxml.FXML
+    private TextField subjectTextField;
+    @javafx.fxml.FXML
+    private TableView tableView;
+    @javafx.fxml.FXML
+    private TableColumn subjectCol;
+    @javafx.fxml.FXML
+    private TextField complaintsTextField;
+    @javafx.fxml.FXML
+    private TextField visitorNameTextField;
+    @javafx.fxml.FXML
+    private TableColumn visitorIDCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void visitorComplaintsButton(ActionEvent actionEvent) {
+    }
+}
