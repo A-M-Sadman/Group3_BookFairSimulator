243587ebecac05710032cf7327e121fa37a480f7
Reya <2211312@iub.edu.bd>
2025-08-07 14:31:08 +0600

    commit 7

--- src/main/java/com/groupthree/group3_bookfairsimulator/Reya/OfficerLogin.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/Reya/OfficerLogin.java
@@ -1,7 +1,27 @@
 package com.groupthree.group3_bookfairsimulator.Reya;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class OfficerLogin
 {
     @javafx.fxml.FXML
+    private TextField officerIdTextField;
+    @javafx.fxml.FXML
+    private ComboBox reportTypeCombo;
+    @javafx.fxml.FXML
+    private TextField officerNameTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void generateButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+}
