af58039988dd30d164c498ac617762d710159db9
tenacioussami <mdfarhanhossainsami@gmail.com>
2025-08-07 11:38:38 +0600

    update fxml 10

--- src/main/java/com/groupthree/group3_bookfairsimulator/Sami/UpdatePublisherProfile.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/Sami/UpdatePublisherProfile.java
@@ -1,7 +1,28 @@
 package com.groupthree.group3_bookfairsimulator.Sami;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class UpdatePublisherProfile
 {
     @javafx.fxml.FXML
+    private TextField publisherIdTextField;
+    @javafx.fxml.FXML
+    private TextField nameTextField;
+    @javafx.fxml.FXML
+    private TextField contactTextField;
+    @javafx.fxml.FXML
+    private TextField descriptionTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void updateProfileButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updatePhotoButton(ActionEvent actionEvent) {
+    }
+}
