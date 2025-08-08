f429c11ab5a528dd63d14785afa68b6a704ebf2c
Reya <2211312@iub.edu.bd>
2025-08-08 22:19:29 +0600

    commit 10

--- src/main/java/com/groupthree/group3_bookfairsimulator/Reya/VolunteerCoordinate.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/Reya/VolunteerCoordinate.java
@@ -1,7 +1,36 @@
 package com.groupthree.group3_bookfairsimulator.Reya;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class VolunteerCoordinate
 {
     @javafx.fxml.FXML
+    private TextField providestallinformationtextfield;
+    @javafx.fxml.FXML
+    private TextArea textareata;
+    @javafx.fxml.FXML
+    private ComboBox availabilitycombo;
+    @javafx.fxml.FXML
+    private TextField volunteeridtextfield;
+    @javafx.fxml.FXML
+    private TextField nametextfield;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void generateonactions(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handlelostandfoundonactions(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backonactions(ActionEvent actionEvent) {
+    }
+}
