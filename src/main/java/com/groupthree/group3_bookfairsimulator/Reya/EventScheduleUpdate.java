f76987d2af3f57d72a597a28c50e451b2e38b801
Reya <2211312@iub.edu.bd>
2025-08-09 22:10:08 +0600

    commit 13

--- src/main/java/com/groupthree/group3_bookfairsimulator/Reya/EventScheduleUpdate.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/Reya/EventScheduleUpdate.java
@@ -1,7 +1,28 @@
 package com.groupthree.group3_bookfairsimulator.Reya;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class EventScheduleUpdate
 {
     @javafx.fxml.FXML
+    private TextField eventidtextfield;
+    @javafx.fxml.FXML
+    private ComboBox eventtypecombobox;
+    @javafx.fxml.FXML
+    private ComboBox locationcombobox;
+    @javafx.fxml.FXML
+    private TextField aurthornametextfield;
+    @javafx.fxml.FXML
+    private Label labeltt;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void updateeventonaction(ActionEvent actionEvent) {
+    }
+}
