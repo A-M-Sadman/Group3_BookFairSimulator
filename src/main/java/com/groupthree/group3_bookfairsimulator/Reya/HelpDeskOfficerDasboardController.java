68b8f72da29d155f3cd9dd7b5aa6f5e9bacc0712
Reya <2211312@iub.edu.bd>
2025-08-10 11:37:16 +0600

    commit 16

--- src/main/java/com/groupthree/group3_bookfairsimulator/Reya/HelpDeskOfficerDasboardController.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/Reya/HelpDeskOfficerDasboardController.java
@@ -1,6 +1,12 @@
 package com.groupthree.group3_bookfairsimulator.Reya;
 
+import com.groupthree.group3_bookfairsimulator.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class HelpDeskOfficerDasboardController
 {
@@ -9,34 +15,66 @@ public class HelpDeskOfficerDasboardController
     }
 
     @javafx.fxml.FXML
-    public void visitorFeedBackButton(ActionEvent actionEvent) {
+    public void visitorFeedBackButton(ActionEvent actionEvent)throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("Reya/visitorFeedBack.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Done");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void provideDirectionButton(ActionEvent actionEvent) {
+    public void provideDirectionButton(ActionEvent actionEvent)throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("Reya/provideDirection.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Book Listing Done");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void officerLoginButton(ActionEvent actionEvent) {
+    public void officerLoginButton(ActionEvent actionEvent)throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("Reya/officerLogin.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Book Listing Done");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void dailyReportButton(ActionEvent actionEvent) {
+    public void dailyReportButton(ActionEvent actionEvent)throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("Reya/dailyReport.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Book Listing Done");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void eventScheduleUpdate(ActionEvent actionEvent) {
+    public void eventScheduleUpdate(ActionEvent actionEvent)throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("Reya/EventSchedule.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Book Listing Done");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void coorDinateButton(ActionEvent actionEvent) {
+    public void coorDinateButton(ActionEvent actionEvent)throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("Reya/coordinateWithOrganizer.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Book Listing Done");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void volunteerCoordinateButton(ActionEvent actionEvent) {
+    public void volunteerCoordinateButton(ActionEvent actionEvent)throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("Reya/volunteerCoordinateButton.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Book Listing Done");
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
-    public void lostandFoundButton(ActionEvent actionEvent) {
+    public void lostandFoundButton(ActionEvent actionEvent)throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("Reya/lostandFound.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setTitle("Book Listing Done");
+        HelloApplication.stage.setScene(scene);
     }
 }
