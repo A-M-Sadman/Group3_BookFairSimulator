6434bc1d141dbb2ac3fd43c01fcea26e7ae7aca6
Reya <2211312@iub.edu.bd>
2025-08-10 12:17:27 +0600

    commit 18

--- src/main/java/com/groupthree/group3_bookfairsimulator/Reya/RealTimeSurvillance.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/Reya/RealTimeSurvillance.java
@@ -2,33 +2,41 @@ package com.groupthree.group3_bookfairsimulator.Reya;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class RealTimeSurvillance
 {
     @javafx.fxml.FXML
-    private TableColumn locationCol;
+    private TableColumn<RealTimeSurvillance,String> locationCol;
     @javafx.fxml.FXML
-    private TableColumn dateCol;
+    private TableColumn<realTimeSurvilance,String> dateCol;
     @javafx.fxml.FXML
-    private ComboBox accessCCTVcombo;
+    private ComboBox<String> accessCCTVcombo;
     @javafx.fxml.FXML
     private DatePicker datePicker;
     @javafx.fxml.FXML
     private TextField cameraTextField;
     @javafx.fxml.FXML
-    private TextField locationTextField;
-    @javafx.fxml.FXML
     private TextField descriptionTextField;
     @javafx.fxml.FXML
-    private TableView table;
+    private TableView<realTimeSurvilance> table;
     @javafx.fxml.FXML
-    private TableColumn caneraCol;
+    private TableColumn<realTimeSurvilance,String> caneraCol;
 
     @javafx.fxml.FXML
     public void initialize() {
+        locationCol.setCellValueFactory(new PropertyValueFactory<>("location"));
+        caneraCol.setCellValueFactory(new PropertyValueFactory<>("cameraId"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+        accessCCTVcombo.getItems().addAll("Bangla Academy Pavilion Zone","Suhrawardy Udyan Stalls Zone","Little Magazine Corner Zone","Children's Corner","TSC","Doyel Chattar","Food Court");
+
     }
 
     @javafx.fxml.FXML
     public void flagButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashboardButton(ActionEvent actionEvent) {
     }
 }
---
+++ src/main/java/com/groupthree/group3_bookfairsimulator/Reya/realTimeSurvilance.java
@@ -0,0 +1,59 @@
+package com.groupthree.group3_bookfairsimulator.Reya;
+
+import com.groupthree.group3_bookfairsimulator.Sami.bookListing;
+
+import java.util.ArrayList;
+
+public class realTimeSurvilance {
+    private String location;
+    private String cameraId;
+    private String date;
+    public static ArrayList<realTimeSurvilance> realTimeSurvilances = new ArrayList<>();
+
+    public realTimeSurvilance(String location, String cameraId, String date) {
+        this.location = location;
+        this.cameraId = cameraId;
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCameraId() {
+        return cameraId;
+    }
+
+    public void setCameraId(String cameraId) {
+        this.cameraId = cameraId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public static ArrayList<realTimeSurvilance> getRealTimeSurvilances() {
+        return realTimeSurvilances;
+    }
+
+    public static void setRealTimeSurvilances(ArrayList<realTimeSurvilance> realTimeSurvilances) {
+        realTimeSurvilance.realTimeSurvilances = realTimeSurvilances;
+    }
+
+    @Override
+    public String toString() {
+        return "realTimeSurvilance{" +
+                "location='" + location + '\'' +
+                ", cameraId='" + cameraId + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
