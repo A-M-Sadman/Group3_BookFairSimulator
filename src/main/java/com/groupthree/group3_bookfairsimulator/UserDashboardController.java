712f766930a8fc818bd47518891f369cb4952628
tenacioussami <mdfarhanhossainsami@gmail.com>
2025-08-11 23:26:10 +0600

    update fxml 16

--- src/main/java/com/groupthree/group3_bookfairsimulator/UserDashboardController.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/UserDashboardController.java
@@ -1,7 +1,22 @@
 package com.groupthree.group3_bookfairsimulator;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class UserDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void logout(ActionEvent actionEvent) throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("Login.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setScene(scene);
+    }
+}
