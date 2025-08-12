374e0c60537695e52911327859c26d957eafc415
Reya <2211312@iub.edu.bd>
2025-08-12 10:20:29 +0600

    commit 22

--- src/main/java/com/groupthree/group3_bookfairsimulator/Reya/ManageSequrityClearence.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/Reya/ManageSequrityClearence.java
@@ -1,29 +1,68 @@
 package com.groupthree.group3_bookfairsimulator.Reya;
 
+import com.groupthree.group3_bookfairsimulator.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class ManageSequrityClearence
 {
     @javafx.fxml.FXML
     private TextField idTextField;
     @javafx.fxml.FXML
-    private ComboBox clearanceTextField;
-    @javafx.fxml.FXML
     private TextField nameTextField;
     @javafx.fxml.FXML
-    private ComboBox roleTextField;
+    private Label secuirityClearanceLabel;
+    @javafx.fxml.FXML
+    private ComboBox<String> roleComboBox;
+    @javafx.fxml.FXML
+    private ComboBox<String> clearanceCombo;
 
     @javafx.fxml.FXML
     public void initialize() {
+        roleComboBox.getItems().addAll("Gate Security officer","Patrol Officer","Crowd control officer","FireFighting Officer","Medical Officer");
+        clearanceCombo.getItems().addAll("General Access – Public Areas", "Zone Supervisor – Specific Zones",
+                "Restricted Area Access – Backstage / Storae","Emergency Response – Fire/Medical/Security Threat","VIP Escort & Protection");
+
     }
 
     @javafx.fxml.FXML
-    public void backButton(ActionEvent actionEvent) {
+    public void backButton(ActionEvent actionEvent)throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("Reya/securityOfficerDashboard.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
     public void manageButton(ActionEvent actionEvent) {
+        if (idTextField.getText().isEmpty()){
+            secuirityClearanceLabel.setText("Please Provide your ID");
+            return;
+        }
+        if (nameTextField.getText().isEmpty()){
+            secuirityClearanceLabel.setText("Please provide your name");
+            return;
+        }
+        if (clearanceCombo.getValue() == null){
+            secuirityClearanceLabel.setText("Please provide clearance");
+            return;
+        }
+        if (roleComboBox.getValue() == null){
+            secuirityClearanceLabel.setText("Please provide role");
+            return;
+        }
+        clearanceCombo.getSelectionModel().clearSelection();
+        roleComboBox.getSelectionModel().clearSelection();
+        idTextField.clear();
+
+        nameTextField.clear();
+        secuirityClearanceLabel.setText("Successful!");
+
     }
 }
