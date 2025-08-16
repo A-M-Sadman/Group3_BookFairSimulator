9ed93b214e4256d752082a4c20f036482baf9695
Reya <2211312@iub.edu.bd>
2025-08-16 12:43:20 +0600

    commit 30

--- src/main/java/com/groupthree/group3_bookfairsimulator/Reya/DailyReport.java
+++ src/main/java/com/groupthree/group3_bookfairsimulator/Reya/DailyReport.java
@@ -1,7 +1,23 @@
 package com.groupthree.group3_bookfairsimulator.Reya;
 
+import com.groupthree.group3_bookfairsimulator.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class DailyReport
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void dashBoardButton(ActionEvent actionEvent)throws IOException {
+        AnchorPane root = FXMLLoader.load(HelloApplication.class.getResource("Reya/HelpDeskOfficerDasboard.fxml"));
+        Scene scene = new Scene(root);
+        HelloApplication.stage.setScene(scene);
+    }
+}
